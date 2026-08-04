bd289d64eab395e274f196cb073fc0f4d115f2a3
ouob123 <kyjin320@sookmyung.ac.kr>
2026-08-04 16:09:45 +0900

    feat: 마이페이지 수정 API 추가

--- src/main/java/com/aivle/sellon/domain/mypage/controller/MyPageController.java
+++ src/main/java/com/aivle/sellon/domain/mypage/controller/MyPageController.java
@@ -1,16 +1,20 @@
 package com.aivle.sellon.domain.mypage.controller;
 
+import com.aivle.sellon.domain.mypage.dto.request.MyPageUpdateRequest;
 import com.aivle.sellon.domain.mypage.dto.response.CompanyKeyResponse;
 import com.aivle.sellon.domain.mypage.dto.response.MyPageResponse;
 import com.aivle.sellon.domain.mypage.service.MyPageService;
 import com.aivle.sellon.global.common.ApiResponse;
 import com.aivle.sellon.global.security.principal.UserPrincipal;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +30,14 @@ public class MyPageController {
             @AuthenticationPrincipal UserPrincipal principal
     ) {
         return ApiResponse.ok(myPageService.getMyPage(principal));
+    }
+
+    @PatchMapping
+    public ResponseEntity<ApiResponse<MyPageResponse>> updateMyPage(
+            @AuthenticationPrincipal UserPrincipal principal,
+            @Valid @RequestBody MyPageUpdateRequest request
+    ) {
+        return ApiResponse.ok(myPageService.updateMyPage(principal, request));
     }
 
     @PostMapping("/company-key")
