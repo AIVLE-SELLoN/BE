9abdf424e229c8b9a3fa234b417fe5a7a6de6f61
whitegyuri <bch0714@naver.com>
2026-08-20 10:15:52 +0900

    feat: 아이디/ 비밀번호 찾기 로직 추가

--- src/main/java/com/aivle/sellon/domain/user/repository/UserRepository.java
+++ src/main/java/com/aivle/sellon/domain/user/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.aivle.sellon.domain.user.repository;
 
+import com.aivle.sellon.domain.company.entity.Company;
 import com.aivle.sellon.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
+
+    // 아이디 찾기 - 회사명 + 사용자 이름으로 조회 (동명이인 있으면 가장 먼저 가입한 계정 기준)
+    Optional<User> findFirstByNameAndCompanyAndDeletedAtIsNullOrderByIdAsc(String name, Company company);
 }
